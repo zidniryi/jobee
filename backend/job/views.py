eacffe5475f8f479217956cc15755a76a8d350d7
zidniryi123 <zidniryi123@gmail.com>
2023-05-15 19:08:16 +0700

    delete jobs

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Job
 from .serializers import JobSerializer
 
 
 # Create your views here.
-
-# Create your views here.
-
 @api_view(['GET'])
 def getAllJobs(request):
 
@@ -18,3 +17,51 @@ def getAllJobs(request):
     serializer = JobSerializer(jobs, many=True)
     return Response(serializer.data)
     
+@api_view(['GET'])
+def getJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    serializer = JobSerializer(job, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def newJob(request):
+    data = request.data
+    
+    job = Job.objects.create(**data)
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.position = request.data['position']
+    job.company = request.data['company']
+    
+    job.save()
+    
+    serializer = JobSerializer(job, many=False)    
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def deleteJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    job.delete()
+
+    return Response({ 'message': 'Job is Deleted.' }, status=status.HTTP_200_OK)
+
+
